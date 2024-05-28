b0b0c92c46a735c4683e2392208e4615168fae8e
MilkyQuartz <milkyquartz.2001@gmail.com>
2024-05-28 17:36:26 +0900

    움직이는 발판

--- Assets/Scripts/Item/TrickItemObject.cs
+++ Assets/Scripts/Item/TrickItemObject.cs
@@ -5,31 +5,82 @@ using UnityEngine;
 public class TrickItemObject : MonoBehaviour, IInteractable
 {
     public TrickItemData trickItemData;
+    private bool isMovePadMoving = false;
+    private GameObject playerObject;
 
     public string GetInteractPrompt()
     {
         string str = $"{trickItemData.displayName}\n{trickItemData.description}";
         return str;
     }
-    private void OnTriggerEnter(Collider other)
+
+    private void OnCollisionEnter(Collision collision)
     {
-        if (other.CompareTag("Player") && trickItemData.trickItemType == TrickItemType.JumpPad)
+        if (collision.gameObject.CompareTag("Player"))
         {
-            Rigidbody playerRigidbody = other.GetComponent<Rigidbody>();
+            playerObject = collision.gameObject; 
+
+            Rigidbody playerRigidbody = playerObject.GetComponent<Rigidbody>();
             if (playerRigidbody != null)
             {
-                Vector3 jumpDirection = transform.up; // 점프대의 위 방향으로 플레이어를 튕기기 위한 벡터
-                float jumpForce = trickItemData.jumpForce; // 점프대에서 가해질 힘의 크기
+                if (trickItemData.trickItemType == TrickItemType.JumpPad)
+                {
+                    Vector3 jumpDirection = transform.up;
+                    float jumpForce = trickItemData.jumpForce;
 
-                Debug.Log("점프대랑 충돌");
-                playerRigidbody.AddForce(jumpDirection * jumpForce, ForceMode.Impulse);
+                    Debug.Log("점프대랑 충돌");
+                    playerRigidbody.AddForce(jumpDirection * jumpForce, ForceMode.Impulse);
+                }
+                else if (trickItemData.trickItemType == TrickItemType.MovePad && !isMovePadMoving)
+                {
+                    Debug.Log("전망대랑 충돌");
+                    StartCoroutine(MovePadRoutine());
+
+                    // 플레이어를 발판의 자식으로 설정하여 발판 위에 올라가게 함
+                    playerObject.transform.parent = transform;
+                }
             }
         }
     }
+
+    private void OnCollisionExit(Collision collision)
+    {
+        if (collision.gameObject == playerObject)
+        {
+            // 부모-자식 관계를 해제
+            playerObject.transform.parent = null;
+            playerObject = null;
+        }
+    }
+
+    private IEnumerator MovePadRoutine()
+    {
+        isMovePadMoving = true;
+        float distanceMoved = 0f;
+
+        while (distanceMoved < trickItemData.moveDistance)
+        {
+            float step = trickItemData.moveSpeed * Time.deltaTime;
+            transform.Translate(Vector3.up * step);
+            distanceMoved += step;
+            yield return null;
+        }
+
+        yield return new WaitForSeconds(1f); 
+
+        while (distanceMoved > 0f)
+        {
+            float step = trickItemData.moveSpeed * Time.deltaTime;
+            transform.Translate(Vector3.down * step);
+            distanceMoved -= step;
+            yield return null;
+        }
+
+        isMovePadMoving = false;
+    }
+
     public void OnInteract()
     {
 
     }
-
-
 }
--- Assets/Scripts/ScriptableObject/TrickItemData.cs
+++ Assets/Scripts/ScriptableObject/TrickItemData.cs
@@ -1,19 +1,25 @@
-using UnityEngine;
+    using UnityEngine;
 
-public enum TrickItemType
-{
-    JumpPad
-}
+    public enum TrickItemType
+    {
+        JumpPad,
+        MovePad
+    }
 
-[CreateAssetMenu(fileName = "TrickItem", menuName = "Trick New Item")]
-public class TrickItemData : ScriptableObject
-{
-    [Header("Info")]
-    public string displayName;
-    public string description;
-    public TrickItemType trickItemType;
+    [CreateAssetMenu(fileName = "TrickItem", menuName = "Trick New Item")]
+    public class TrickItemData : ScriptableObject
+    {
+        [Header("Info")]
+        public string displayName;
+        public string description;
+        public TrickItemType trickItemType;
 
-    [Header("JumpPad")]
-    public GameObject jumpPadPrefab;
-    public float jumpForce;
+        [Header("JumpPad")]
+        public GameObject jumpPadPrefab;
+        public float jumpForce;
+
+        [Header("MovePad")]
+        public GameObject movePadPrefab;
+        public float moveSpeed;
+        public float moveDistance;
 }
